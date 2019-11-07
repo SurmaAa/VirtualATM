fa57a640c7cc0695b3e3f3dede32e3d2579e896f
SurmaAa <anondebilwkazdymfredzie@gmail.com>
2019-11-07 18:38:23 +0100

    Updates

---
+++ precache-manifest.5b27248457adf0f2d9579181b58c9ce2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c84eb9441f40a07f46738f309de43a2",
+    "url": "/VirtualATM/index.html"
+  },
+  {
+    "revision": "2a272e09001df41a11d1",
+    "url": "/VirtualATM/static/css/main.71ecaf01.chunk.css"
+  },
+  {
+    "revision": "67b3bde182787e791fce",
+    "url": "/VirtualATM/static/js/2.9e940e50.chunk.js"
+  },
+  {
+    "revision": "2a272e09001df41a11d1",
+    "url": "/VirtualATM/static/js/main.1f08559d.chunk.js"
+  },
+  {
+    "revision": "8f92963ec8740d88af3e",
+    "url": "/VirtualATM/static/js/runtime-main.3db10576.js"
+  },
+  {
+    "revision": "14c590d124662eb50efa4c00f027b79c",
+    "url": "/VirtualATM/static/media/fa-brands-400.14c590d1.eot"
+  },
+  {
+    "revision": "3e1b2a654a784ceb385157140b4ccd71",
+    "url": "/VirtualATM/static/media/fa-brands-400.3e1b2a65.woff2"
+  },
+  {
+    "revision": "5e8aa9ea0ebcd2218178f554cdd8e6b0",
+    "url": "/VirtualATM/static/media/fa-brands-400.5e8aa9ea.ttf"
+  },
+  {
+    "revision": "bfa9c38bd6081dafe7278dedc4aad1d9",
+    "url": "/VirtualATM/static/media/fa-brands-400.bfa9c38b.svg"
+  },
+  {
+    "revision": "df02c782834b113d605d8329368737b4",
+    "url": "/VirtualATM/static/media/fa-brands-400.df02c782.woff"
+  },
+  {
+    "revision": "285a9d2a28886ad64c4e45cbd733cf7c",
+    "url": "/VirtualATM/static/media/fa-regular-400.285a9d2a.ttf"
+  },
+  {
+    "revision": "5623624dd1b017c66c29d1ac69cdcfa3",
+    "url": "/VirtualATM/static/media/fa-regular-400.5623624d.woff"
+  },
+  {
+    "revision": "95f13e0be408d117bd3d9b366084a3ef",
+    "url": "/VirtualATM/static/media/fa-regular-400.95f13e0b.svg"
+  },
+  {
+    "revision": "aa66d0e0e38c75666e98db33abae955e",
+    "url": "/VirtualATM/static/media/fa-regular-400.aa66d0e0.eot"
+  },
+  {
+    "revision": "ac21cac3f22cc9642f5af32e0c750797",
+    "url": "/VirtualATM/static/media/fa-regular-400.ac21cac3.woff2"
+  },
+  {
+    "revision": "3ded831d708bf90b2da86756551b5c1c",
+    "url": "/VirtualATM/static/media/fa-solid-900.3ded831d.woff"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "/VirtualATM/static/media/fa-solid-900.42e1fbd2.eot"
+  },
+  {
+    "revision": "6ed5e3bc9018d2a03e2f1fadbf8a0a4a",
+    "url": "/VirtualATM/static/media/fa-solid-900.6ed5e3bc.svg"
+  },
+  {
+    "revision": "896e20e26ad07dc63c9526ed814117e9",
+    "url": "/VirtualATM/static/media/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "d6d8d5da9214dc7d46b297672a602d55",
+    "url": "/VirtualATM/static/media/fa-solid-900.d6d8d5da.woff2"
+  }
+]);
